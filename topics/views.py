0f987f58e868e7347fa407595335df7e9f431445
PraseedaSaripalle <73073142+PraseedaSaripalle@users.noreply.github.com>
2022-04-15 17:20:18 +0530

    Update views.py

--- topics/views.py
+++ topics/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .services import *
+
+def upload_topics(request):
+    path="D:\Ml Project\Enligence Internship\datasets\TopicsTaxonomy.xlsx"
+    sheet_value='CS'
+    read_excel(path,sheet_value)
+    return render(request,'upload.html')
