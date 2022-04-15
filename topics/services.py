14f2a451a4b0c5b222f104f371fe51472c1a2e0e
PraseedaSaripalle <73073142+PraseedaSaripalle@users.noreply.github.com>
2022-04-15 17:18:07 +0530

    Update services.py

--- topics/services.py
+++ topics/services.py
@@ -4,3 +4,21 @@ services to import data from excel files
 def import_xl(path_to_file):
     pass
 """
+
+import pandas as pd
+from .models import Topic
+
+def read_excel(path,sheet_value):
+    df = pd.read_excel (path,sheet_name=sheet_value)
+    for row in range(len(df.index)):
+        topic=Topic.objects.create(topic=df['Topic'][row])
+        topic.save()
+        subtopic=df['SubTopics'][row]
+        subtopic_seperaetd=subtopic.split(",")
+        subtopic_count=0
+        for subtopic in subtopic_seperaetd:
+            subtopic=subtopic.strip()
+            subtopic_name=Topic.objects.create(topic=subtopic)
+            subtopic_name.save()
+            topic.children.add(subtopic_name)
+            subtopic_count=subtopic_count+1
