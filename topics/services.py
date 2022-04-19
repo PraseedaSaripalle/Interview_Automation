bb4d7bec1cede57bbabcc9b5bd2fa192b72b22b7
abhinavdayal <abhinav.dayal@hotmail.com>
2022-04-19 12:07:42 +0530

    refactored to DRY and performant version

--- topics/services.py
+++ topics/services.py
@@ -7,51 +7,49 @@ def import_xl(path_to_file):
 
 import pandas as pd
 from .models import Topic
+from django.core.exceptions import ValidationError
+
+def get_or_create_topic(topic_name):
+    """
+    fetched a topic (case insensitive) if exists, else creates one
+
+    @string topic_name: name of the topic
+    """
+    topic_name = topic_name.strip()
+
+    try:
+        topic, _ = Topic.objects.get_or_create(topic=topic_name)
+    except ValidationError:
+        # topic already exists
+        topic = Topic.objects.filter(topic__iexact=topic_name).first()
+
+    return topic
+
 
 def read_excel(path,sheet_value):
-    #function used to read the excel/csv file. path and the name of the sheet are to be given as the parameters.
-    df = pd.read_excel (path,sheet_name=sheet_value)
-    #the pandas function is used to directly import the excel files.
+    """
+    function used to read the excel/csv file.
+    
+    @string path: path to the excel file
+    @string sheet_value: sheet name in the excel file
+
+    @returns:nothing
+
+    comments:
+    Each row of the excel file has a format
+    col1 (Topic)        |   col2 (SubTopics)
+    -----------------------------------------------------
+    parent topic |  comma seaparted child topics 
+    """
+    
+    # create a pandas data frame from the sheet of the excel file
+    df = pd.read_excel(path,sheet_name=sheet_value)
+
+    #iterating through the length of the rows that are present in the dataframe
     for row in range(len(df.index)):
-        #iterating through the length of the rows that are present in the dataframe
-        #print(row)
-        print("df['Topic'][row]",df['Topic'][row])
-        tlist=Topic.objects.filter(topic__iexact=df['Topic'][row])
-        #filtering topic, if it had the same topicname
-        nooftopics=tlist.count()
-        #counting the topic query set
-        print("nooftopics",nooftopics)
-        topic=None
-        if(nooftopics==0):
-            #checking for the condition if topic count is 0
-            topic=Topic.objects.create(topic=df['Topic'][row])
-            #if 0, then enter that topic
-            topic.save()
-            #save the entered topic
-            print("saved topic",topic)
-        topic = Topic.objects.get(topic=df['Topic'][row])
-        #get all the entered topics and put them in topic. 
-        subtopic=df['SubTopics'][row]
-        #get subtopics from the excel
-        subtopic_seperaetd=subtopic.split(",")
-        #since the children of a topic are stored in as comma seperated values, we need to split those values. The resultant is in form of a list.
-        subtopic_count=0
-        for subtopiccolumn in subtopic_seperaetd:
-            subtopiccolumn=subtopiccolumn.strip()
-            #strip is going to remove the unwanted spaces before and after the word.
-            print("subtopiccolumn",subtopiccolumn)
-            tlist=Topic.objects.filter(topic__iexact=subtopiccolumn)
-            #check if the topic name matches to list of topics already entered
-            nooftopics=tlist.count()
-            print("nosubtopics",nooftopics)
-            
-            subtopic_name=None
-            if(nooftopics==0):
-                subtopic_name=Topic.objects.create(topic=subtopiccolumn)
-                subtopic_name.save()
-                print("saved subtopic",subtopic_name)
-                print("before adding subtopic to children")
-            subtopic_name=Topic.objects.get(topic=subtopiccolumn)
-            print("subtopic_name after search",subtopic_name.topic)
-            topic.children.add(subtopic_name)
-            subtopic_count=subtopic_count+1
+        # get or creqate parent topic
+        parent_topic = get_or_create_topic(df['Topic'][row].strip())
+
+        # Get and assign the subtopics
+        parent_topic.children.add(*map(get_or_create_topic, df['SubTopics'][row].split(",")))
+        
